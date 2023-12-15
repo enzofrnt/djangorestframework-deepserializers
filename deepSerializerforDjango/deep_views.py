b1486eb0ef8dcedeeca09af7847712cbc1862439
Horou <nicolas.nemouthe@gmail.com>
2023-12-15 18:44:31 +0100

    Now with more comments

--- deepSerializerforDjango/deep_views.py
+++ deepSerializerforDjango/deep_views.py
@@ -1,7 +1,7 @@
 from rest_framework.utils import model_meta
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-from .deep_serializers import DeepSerializer
+from .serializers import DeepSerializer
 
 
 ########################################################################################################################
@@ -10,10 +10,20 @@ from .deep_serializers import DeepSerializer
 
 
 class ReadOnlyDeepViewSet(ReadOnlyModelViewSet):
+    """
+    A unique viewset for all your need of deep read and deep write, made easy
+
+    Read only version
+    """
     _viewsets = {}
     _mode = "Read"
 
     def __init_subclass__(cls, **kwargs):
+        """
+        Used to save the important information like:
+            -> all the viewset inheriting DeepViewSet and avoid creating already created viewset
+            -> all the possible filter_fields to 'filter by' or 'order by' for this viewset
+        """
         super().__init_subclass__(**kwargs)
         if cls.queryset:
             model = cls.queryset.model
@@ -26,6 +36,12 @@ class ReadOnlyDeepViewSet(ReadOnlyModelViewSet):
 
     @classmethod
     def build_filter_fields(cls, parent_model, exclude_models):
+        """
+        Is used to create the list of all the possible field inside this view,
+            Used to check if a filter or order_by params is valid for the request
+
+        Should never be called alone, because generate the list recursively through all the nested models
+        """
         exclude_set = {n for n in model_meta.get_field_info(parent_model).reverse_relations if n.endswith("_set")}
         prefetch_related = []
         for field_relation in parent_model._meta.get_fields():
@@ -39,13 +55,33 @@ class ReadOnlyDeepViewSet(ReadOnlyModelViewSet):
 
     @classmethod
     def init_router(cls, router, models: list):
+        """
+        models: list of model to register in the router
+
+        Is used to create all the viewset for the model in models and register them inside the router
+        """
         for model in models:
             router.register(model.__name__, cls.get_view(model), basename=model.__name__)
 
     def get_serializer_class(self):
+        """
+        Used to get the seralizer class for the current viewset for its mode,
+            No mode mean it will get the main serializer for the model
+        """
         return DeepSerializer.get_serializer(self.queryset.model, mode=self._mode)
 
     def get_queryset(self):
+        """
+        Is used to modify the queryset to get exactly what you want
+
+        Filtering is made with 'field_name=value'. (example: /?label=foo&name=bar)
+        Filter by nested model field with 'field_name__field_name=value'. (example: /?group__label=bar)
+        Sorting is made with 'order_by' like 'order_by=field_name'. (example: /?order_by=foo,bar)
+        Display deeper model with 'depth' like 'depth=depth_level'. (example: /?depth=5)
+        Remove deeper model with 'exclude' like 'exclude=foo' or 'exclude=foo,bar'
+        Exclude nested model of nested model like 'exclude=bar__foo,bar__user__group,bar__user__comments'
+        And you can do it all at once. (example: /?depth=10&order_by=foo&label=bar&group=bar)
+        """
         params = self.request.query_params
         serializer = self.get_serializer_class()
         serializer.Meta.depth = int(params.get("depth", self.depth))
@@ -59,6 +95,15 @@ class ReadOnlyDeepViewSet(ReadOnlyModelViewSet):
 
     @classmethod
     def get_view(cls, _model, mode: str = ""):
+        """
+        Create a viewset for the _model and its mode if it does not exist, else it gets the viewset back
+        You can create your own viewset that inherit DeepViewSet, and it will be used when called upon
+        If your viewset is only used in a specific use-case, write it in the mode
+
+        _model: Contain the model related to the viewset wanted
+        mode: Contain the use that this viewset will be used for,
+            if empty, it will be the main viewset for this model
+        """
         if mode + _model.__name__ not in cls._viewsets:
             class CommonViewSet(cls):
                 _mode = mode
@@ -83,6 +128,11 @@ class ReadOnlyDeepViewSet(ReadOnlyModelViewSet):
 
 
 class DeepViewSet(ReadOnlyDeepViewSet, ModelViewSet):
+    """
+    A unique viewset for all your need of deep read and deep write, made easy
+
+    Read and Write version
+    """
     _mode = ""
 
 ########################################################################################################################
