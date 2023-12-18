45f3dfd2bcfa16b703b25e5e396e68ed55ed41bd
Horou <nicolas.nemouthe@gmail.com>
2023-12-18 14:28:41 +0100

    Update and rename __init__.py to __init__.py

--- deepserializer/__init__.py
+++ deepserializer/__init__.py
@@ -3,4 +3,4 @@ This module is to make improt easier
 """
 
 from .deep_serializers import DeepSerializer
-from .deep_views import ReadOnlyDeepViewSet
+from .deep_views import ReadOnlyDeepViewSet, DeepViewSet
