aa96bdf82b6b275f8ccdbc151c18183f3c0af92f
Fournet Enzo <enzofournet31@gmail.com>
2023-12-18 11:36:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ This model is used to create the package
 
 from setuptools import setup
 
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name='djangorestframework-deepserializer',
     version='0.1',
@@ -17,7 +13,6 @@ setup(
         'djangorestframework',
     ],
     description='A package to create deep serializer for django rest framework',
-    long_description=long_description,
     author='Horou and Enzo_frnt',
     url='https://github.com/Horou/djangorestframework-deepserializer',
     keywords=["Django", "Django REST Framework", "Deep", "Depth", "serializer", "viewset", "nested", "nested serializer"],
