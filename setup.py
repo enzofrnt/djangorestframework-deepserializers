8bc34355050ca1543179e58cc815df4b9d7ad7aa
Horou <nicolas.nemouthe@gmail.com>
2023-12-21 18:44:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='djangorestframework-deepserializer',
-    version='0.1.5',
+    version='0.1.7',
     packages=['deepserializer'],
     install_requires=[
         'Django',
